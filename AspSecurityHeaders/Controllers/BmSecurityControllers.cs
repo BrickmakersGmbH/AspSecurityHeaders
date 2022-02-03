b4f390ce9fa896980791a841e6879eeb925b0116
Felix Barz <felix.barz@brickmakers.de>
2022-02-03 08:05:43 +0100

    [SE-185] add security controllers xmldoc
    includes the CSP controller

--- AspSecurityHeaders/Controllers/BmSecurityControllers.cs
+++ AspSecurityHeaders/Controllers/BmSecurityControllers.cs
@@ -1,10 +1,29 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Brickmakers.AspSecurityHeaders.Controllers;
 
+/// <summary>
+///     Extensions to <see cref="IMvcBuilder" /> to add security controllers
+/// </summary>
 public static class BmSecurityControllers
 {
+    /// <summary>
+    ///     Adds all currently available security controllers to the application. Currently, those are:
+    ///     <ul>
+    ///         <li>
+    ///             <see cref="CspReportController" />
+    ///         </li>
+    ///     </ul>
+    /// </summary>
+    /// <param name="mvcBuilder">The <see cref="IMvcBuilder" /> to add the controllers to.</param>
+    /// <returns>The mvcBuilder that was passed as this.</returns>
+    /// <remarks>
+    ///     This only adds the controllers to the MVC context. In order for the to actually be available,
+    ///     <see cref="ControllerEndpointRouteBuilderExtensions.MapControllers">IEndpointRouteBuilder.MapControllers()</see>
+    ///     must also be called in your StartUp (See README for more details).
+    /// </remarks>
     public static IMvcBuilder AddSecurityControllers(this IMvcBuilder mvcBuilder)
     {
         return mvcBuilder
--- AspSecurityHeaders/Controllers/Models/CspModels.cs
+++ AspSecurityHeaders/Controllers/Models/CspModels.cs
@@ -4,18 +4,30 @@ using System.Text.Json.Serialization;
 
 namespace Brickmakers.AspSecurityHeaders.Controllers.Models;
 
+/// <summary>
+///     A Request sent to the <see cref="CspReportController" /> containing a CSP report.
+/// </summary>
 public class CspReportRequest
 {
+    /// <summary>
+    ///     The <see cref="CspReport" /> of the request
+    /// </summary>
     [Required]
     [JsonPropertyName("csp-report")]
     public CspReport CspReport { get; set; } = null!;
 }
 
+/// <summary>
+///     A CSP-Report as it is sent by the browsers. This model maps the JSON-Fields of such a request. See
+///     <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy">Content-Security-Policy</a>
+/// </summary>
 public class CspReport
 {
-    [JsonPropertyName("document-uri")] public string? DocumentUri { get; set; }
+    [JsonPropertyName("document-uri")]
+    public string? DocumentUri { get; set; }
 
-    [JsonPropertyName("referrer")] public string? Referrer { get; set; }
+    [JsonPropertyName("referrer")]
+    public string? Referrer { get; set; }
 
     [JsonPropertyName("violated-directive")]
     public string? ViolatedDirective { get; set; }
@@ -23,16 +35,25 @@ public class CspReport
     [JsonPropertyName("effective-directive")]
     public string? EffectiveDirective { get; set; }
 
-    [JsonPropertyName("original-policy")] public string? OriginalPolicy { get; set; }
+    [JsonPropertyName("original-policy")]
+    public string? OriginalPolicy { get; set; }
 
-    [JsonPropertyName("blocked-uri")] public string? BlockedUri { get; set; }
+    [JsonPropertyName("blocked-uri")]
+    public string? BlockedUri { get; set; }
 
-    [JsonPropertyName("status-code")] public int? StatusCode { get; set; }
+    [JsonPropertyName("status-code")]
+    public int? StatusCode { get; set; }
 
-    [JsonPropertyName("disposition")] public string? Disposition { get; set; }
+    [JsonPropertyName("disposition")]
+    public string? Disposition { get; set; }
 
-    [JsonPropertyName("script-sample")] public string? ScriptSample { get; set; }
+    [JsonPropertyName("script-sample")]
+    public string? ScriptSample { get; set; }
 
+    /// <summary>
+    ///     Generates a dictionary containing all the fields of the CSP report.
+    /// </summary>
+    /// <returns>A dictionary with all fields of the CSP.</returns>
     public IDictionary<string, string?> AsAttributes()
     {
         return GetType().GetProperties().ToDictionary(
@@ -40,6 +61,7 @@ public class CspReport
             prop => prop.GetValue(this)?.ToString());
     }
 
+    /// <inheritdoc cref="object.ToString()" />
     public override string ToString()
     {
         return
