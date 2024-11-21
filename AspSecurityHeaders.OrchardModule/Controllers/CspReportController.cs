32fca34bdf0372c62ee3b8a453c591b584020350
Felix Barz <felix.barz@brickmakers.de>
2024-11-21 13:16:42 +0100

    [SE-203] update orchard core to 2.0

--- AspSecurityHeaders.OrchardModule/Controllers/CspReportController.cs
+++ AspSecurityHeaders.OrchardModule/Controllers/CspReportController.cs
@@ -1,5 +1,5 @@
 using System.ComponentModel.DataAnnotations;
-using Brickmakers.AspSecurityHeaders.OrchardModule.Controllers.Models;
+using Brickmakers.AspSecurityHeaders.Controllers.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
