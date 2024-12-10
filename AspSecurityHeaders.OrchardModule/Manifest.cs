b8336d96f143c1cd5a555da1aa31ed51da9bc09e
Felix Barz <felix.barz@brickmakers.de>
2024-12-10 07:39:12 +0100

    [SE-203] bump version

--- AspSecurityHeaders.OrchardModule/Manifest.cs
+++ AspSecurityHeaders.OrchardModule/Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "Brickmakers.AspSecurityHeaders.OrchardModule",
     Author = "BRICKMAKERS AG",
     Website = "https://www.brickmakers.de",
-    Version = "2.5.2",
+    Version = "2.6.1",
     Description = "An Orchard Core MVC module to automatically configure secure HTTP-Headers.",
     Category = "BRICKS",
     Tags = new[] { "Security", "Headers", "CSP" }
--- bump-version.sh
+++ bump-version.sh
@@ -2,19 +2,15 @@
 
 new_version=${1?:First parameter must be the new version}
 
-csproj_files=(
-  "AspSecurityHeaders/AspSecurityHeaders.csproj"
-  "AspSecurityHeaders.OrchardModule/AspSecurityHeaders.OrchardModule.csproj"
-  "AspSecurityHeaders.Generators/AspSecurityHeaders.Generators.csproj"
-) 
-for csproj_file in "${csproj_files[@]}"; do
-  sed -i '' -E "s#<(Assembly|File|Package)Version>[0-9\.]+</(Assembly|File|Package)Version>#<\1Version>$new_version</\2Version>#g" "$csproj_file"
-done
+sed \
+  -i '' \
+  -E "s#<(Assembly|File|Package|Informational)Version>[0-9\.]+</(Assembly|File|Package|Informational)Version>#<\1Version>$new_version</\2Version>#g" \
+  Directory.Build.targets
 
-manifest_files=("AspSecurityHeaders.OrchardModule/Manifest.cs")
-for manifest_file in "${manifest_files[@]}"; do
-  sed -i '' -E "s/Version = \"[0-9\.]+\",/Version = \"$new_version\",/" "$manifest_file"
-done
+sed \
+  -i '' \
+  -E "s/Version = \"[0-9\.]+\",/Version = \"$new_version\",/" \
+  AspSecurityHeaders.OrchardModule/Manifest.cs
 
 find . -name "packages.lock.json" -exec rm {} \;
 dotnet restore --use-lock-file
